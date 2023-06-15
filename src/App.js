10ee4851f988d4d74378bc912bd842afc7b6ab17
Olasunkanmi <abdulsalamquadri999@gmail.com>
2023-06-15 23:47:12 +0100

    initialize project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function App() {
+    return (
+      <BrowserRouter>
+        <Switch>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+  
+  export default App;
